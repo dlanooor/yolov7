05d06051f2a2aef5f95de3bc1ddc36b0e975cc84
Ronald Grant <63400878+dlanooor@users.noreply.github.com>
2023-06-12 23:11:04 +0700

    no onnx export

--- export.py
+++ export.py
@@ -113,93 +113,93 @@ if __name__ == '__main__':
     except Exception as e:
         print('TorchScript-Lite export failure: %s' % e)
 
-    # ONNX export
-    try:
-        import onnx
+#     # ONNX export
+#     try:
+#         import onnx
 
-        print('\nStarting ONNX export with onnx %s...' % onnx.__version__)
-        f = opt.weights.replace('.pt', '.onnx')  # filename
-        model.eval()
-        output_names = ['classes', 'boxes'] if y is None else ['output']
-        dynamic_axes = None
-        if opt.dynamic:
-            dynamic_axes = {'images': {0: 'batch', 2: 'height', 3: 'width'},  # size(1,3,640,640)
-             'output': {0: 'batch', 2: 'y', 3: 'x'}}
-        if opt.dynamic_batch:
-            opt.batch_size = 'batch'
-            dynamic_axes = {
-                'images': {
-                    0: 'batch',
-                }, }
-            if opt.end2end and opt.max_wh is None:
-                output_axes = {
-                    'num_dets': {0: 'batch'},
-                    'det_boxes': {0: 'batch'},
-                    'det_scores': {0: 'batch'},
-                    'det_classes': {0: 'batch'},
-                }
-            else:
-                output_axes = {
-                    'output': {0: 'batch'},
-                }
-            dynamic_axes.update(output_axes)
-        if opt.grid:
-            if opt.end2end:
-                print('\nStarting export end2end onnx model for %s...' % 'TensorRT' if opt.max_wh is None else 'onnxruntime')
-                model = End2End(model,opt.topk_all,opt.iou_thres,opt.conf_thres,opt.max_wh,device,len(labels))
-                if opt.end2end and opt.max_wh is None:
-                    output_names = ['num_dets', 'det_boxes', 'det_scores', 'det_classes']
-                    shapes = [opt.batch_size, 1, opt.batch_size, opt.topk_all, 4,
-                              opt.batch_size, opt.topk_all, opt.batch_size, opt.topk_all]
-                else:
-                    output_names = ['output']
-            else:
-                model.model[-1].concat = True
+#         print('\nStarting ONNX export with onnx %s...' % onnx.__version__)
+#         f = opt.weights.replace('.pt', '.onnx')  # filename
+#         model.eval()
+#         output_names = ['classes', 'boxes'] if y is None else ['output']
+#         dynamic_axes = None
+#         if opt.dynamic:
+#             dynamic_axes = {'images': {0: 'batch', 2: 'height', 3: 'width'},  # size(1,3,640,640)
+#              'output': {0: 'batch', 2: 'y', 3: 'x'}}
+#         if opt.dynamic_batch:
+#             opt.batch_size = 'batch'
+#             dynamic_axes = {
+#                 'images': {
+#                     0: 'batch',
+#                 }, }
+#             if opt.end2end and opt.max_wh is None:
+#                 output_axes = {
+#                     'num_dets': {0: 'batch'},
+#                     'det_boxes': {0: 'batch'},
+#                     'det_scores': {0: 'batch'},
+#                     'det_classes': {0: 'batch'},
+#                 }
+#             else:
+#                 output_axes = {
+#                     'output': {0: 'batch'},
+#                 }
+#             dynamic_axes.update(output_axes)
+#         if opt.grid:
+#             if opt.end2end:
+#                 print('\nStarting export end2end onnx model for %s...' % 'TensorRT' if opt.max_wh is None else 'onnxruntime')
+#                 model = End2End(model,opt.topk_all,opt.iou_thres,opt.conf_thres,opt.max_wh,device,len(labels))
+#                 if opt.end2end and opt.max_wh is None:
+#                     output_names = ['num_dets', 'det_boxes', 'det_scores', 'det_classes']
+#                     shapes = [opt.batch_size, 1, opt.batch_size, opt.topk_all, 4,
+#                               opt.batch_size, opt.topk_all, opt.batch_size, opt.topk_all]
+#                 else:
+#                     output_names = ['output']
+#             else:
+#                 model.model[-1].concat = True
 
-        torch.onnx.export(model, img, f, verbose=False, opset_version=12, input_names=['images'],
-                          output_names=output_names,
-                          dynamic_axes=dynamic_axes)
+#         torch.onnx.export(model, img, f, verbose=False, opset_version=12, input_names=['images'],
+#                           output_names=output_names,
+#                           dynamic_axes=dynamic_axes)
 
-        # Checks
-        onnx_model = onnx.load(f)  # load onnx model
-        onnx.checker.check_model(onnx_model)  # check onnx model
+#         # Checks
+#         onnx_model = onnx.load(f)  # load onnx model
+#         onnx.checker.check_model(onnx_model)  # check onnx model
 
-        if opt.end2end and opt.max_wh is None:
-            for i in onnx_model.graph.output:
-                for j in i.type.tensor_type.shape.dim:
-                    j.dim_param = str(shapes.pop(0))
+#         if opt.end2end and opt.max_wh is None:
+#             for i in onnx_model.graph.output:
+#                 for j in i.type.tensor_type.shape.dim:
+#                     j.dim_param = str(shapes.pop(0))
 
-        # print(onnx.helper.printable_graph(onnx_model.graph))  # print a human readable model
+#         # print(onnx.helper.printable_graph(onnx_model.graph))  # print a human readable model
 
-        # # Metadata
-        # d = {'stride': int(max(model.stride))}
-        # for k, v in d.items():
-        #     meta = onnx_model.metadata_props.add()
-        #     meta.key, meta.value = k, str(v)
-        # onnx.save(onnx_model, f)
+#         # # Metadata
+#         # d = {'stride': int(max(model.stride))}
+#         # for k, v in d.items():
+#         #     meta = onnx_model.metadata_props.add()
+#         #     meta.key, meta.value = k, str(v)
+#         # onnx.save(onnx_model, f)
 
-        if opt.simplify:
-            try:
-                import onnxsim
+#         if opt.simplify:
+#             try:
+#                 import onnxsim
 
-                print('\nStarting to simplify ONNX...')
-                onnx_model, check = onnxsim.simplify(onnx_model)
-                assert check, 'assert check failed'
-            except Exception as e:
-                print(f'Simplifier failure: {e}')
+#                 print('\nStarting to simplify ONNX...')
+#                 onnx_model, check = onnxsim.simplify(onnx_model)
+#                 assert check, 'assert check failed'
+#             except Exception as e:
+#                 print(f'Simplifier failure: {e}')
 
-        # print(onnx.helper.printable_graph(onnx_model.graph))  # print a human readable model
-        onnx.save(onnx_model,f)
-        print('ONNX export success, saved as %s' % f)
+#         # print(onnx.helper.printable_graph(onnx_model.graph))  # print a human readable model
+#         onnx.save(onnx_model,f)
+#         print('ONNX export success, saved as %s' % f)
 
-        if opt.include_nms:
-            print('Registering NMS plugin for ONNX...')
-            mo = RegisterNMS(f)
-            mo.register_nms()
-            mo.save(f)
+#         if opt.include_nms:
+#             print('Registering NMS plugin for ONNX...')
+#             mo = RegisterNMS(f)
+#             mo.register_nms()
+#             mo.save(f)
 
-    except Exception as e:
-        print('ONNX export failure: %s' % e)
+#     except Exception as e:
+#         print('ONNX export failure: %s' % e)
 
     # Finish
     print('\nExport complete (%.2fs). Visualize with https://github.com/lutzroeder/netron.' % (time.time() - t))
